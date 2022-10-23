aa3e6c1d8601de6d9dfb76dc0a0961a1ab4a5456
Sri Teja <sriteja2602@gmail.com>
2022-10-23 11:35:52 +0530

    changed to hashrouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import EditNoteView from '@/views/EditNoteView.vue'
 import HomeView from '@/views/HomeView.vue'
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
   })
 
